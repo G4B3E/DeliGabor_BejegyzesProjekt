a33888dac5f0ab35b5393a29d9c182ef08334d0c
Deli Gabor <email@gmail.com>
2022-09-30 15:33:42 +0200

    commit

--- src/hu/petrik/helloworld_14s/Main.java
+++ src/hu/petrik/helloworld_14s/Main.java
@@ -3,6 +3,7 @@ package hu.petrik.helloworld_14s;
 public class Main {
 
     public static void main(String[] args) {
-
+        Bejegyzes bejegyzes = new Bejegyzes("Gipsz Jakab","poszt");
+      
     }
 }
