fca841fece73f5819df5c9194f07d6d51956ddf1
Deli Gabor <email@gmail.com>
2022-09-30 14:48:27 +0200

    commit

--- src/Bejegyzes.java
+++ src/Bejegyzes.java
@@ -1,3 +1,51 @@
-public class Bejegyzes {
+import java.time.LocalDateTime;
 
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+
+    public Bejegyzes(LocalDateTime letrejott, LocalDateTime szerkesztve) {
+        this.letrejott = letrejott;
+        this.szerkesztve = szerkesztve;
+    }
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+
+    }
 }
+
